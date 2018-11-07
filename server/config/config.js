53d8cf1acf889d74b3e6e80a891e4d16a98f81bd
kevindz10 <kevindiaz712@gmail.com>
2018-11-06 18:36:25 -0600

    restserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,27 @@
 //  Puerto
 // ============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+    
+} else {
+    // urlDB = process.env.MONGO_URI;
+    urlDB ='mongodb://kevind:12345a@ds155263.mlab.com:55263/cafe_node';
+}
+process.env.URLDB = urlDB;
+
+
+
